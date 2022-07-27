96e07fe95f468cdf4f5ad36a2d9d9bee25ff5ace
Isaac Ramos <46172558+IsaacRamos1@users.noreply.github.com>
2022-07-27 10:51:00 -0300

    interface e arquivos

--- expr2022.g4
+++ expr2022.g4
@@ -2,7 +2,8 @@ lexer grammar expr2022;
 
 PR: 'True'|'False'|'for'|'in'|'range'|'None'|'and'|'or'|'not'|'if'|'else'|'elsif'
     |'while'|'do'|'continue'|'break'|'input'|'main'|'def'|'float'|'int'
-    |'bool'|'string'|'class'|'as'|'try'|'catch'|'exception'|'import'|'from'|'return';
+    |'bool'|'string'|'class'|'as'|'try'|'catch'|'exception'|'import'|'from'|'return'
+    |'list'|'dict'|'tuple'|'print';
 
 ID: [a-zA-Z][a-zA-Z0-9]*;
 NUM: [0-9]+[.]?[0-9]*;
--- main.py
+++ main.py
@@ -1,21 +1,68 @@
 from antlr4 import *
 from gen.expr2022 import expr2022
+from tkinter import *
+from tkinter.filedialog import askopenfilename
+from tkinter import ttk
 
-if __name__ == '__main__':
-    print('AntLR com Python: ')
-    # entrada dos dados ---------> implementação do front
-    exp = "for i in range(1,100):"
+def abrirAqruivo():
+    path = askopenfilename(filetypes=[('Python Files', '*.py'), ('Text Files', '*.txt')])
+    if path == '':
+        return
+    with open(path, 'r') as file:
+        code = file.read()
+        editorText.delete('1.0', END)
+        editorText.insert('1.0', code)
 
-    data = InputStream(exp)
+
+def analise():
+    code = editorText.get('1.0', END)
+    data = InputStream(code)
     lexer = expr2022(data)
     tokens = CommonTokenStream(lexer)
-
-    #print(tokens)
-    # adquire os tokens
-
     list_tokens = lexer.getAllTokens()
+
+    file = open('lexica.txt', 'w+')
+    file.write("============================\n")
+    file.write("{:<15} {:<8}\n".format('Token', 'Type'))
+    file.write("============================\n")
+
     for token in list_tokens:
         nomeEtoken = expr2022.symbolicNames[token.type]
-        print(token.text + " " + nomeEtoken)
+        file.write("\n{:15} {:<8}".format(token.text, nomeEtoken))
+
+    file.seek(0)
+    out.insert('1.0', 'Process finished with exit code 0\n' + file.read())
     lexer.reset()
+    file.close()
+
+if __name__ == '__main__':
+    window = Tk()
+    window.title('Lexer Analyzer')
+    window.geometry('500x500')
+    window.resizable(True, True)
+
+    #print(window.winfo_reqheight())
+    #print(window.winfo_reqheight(), window.winfo_reqwidth())
+    editorText = Text(height=10, font=("Consolas", 18))
+    editorText.config(bg='#333333', fg='#b08102', insertbackground='white')
+    editorText.pack(fill=X)
+
+    out = Text(height=window.winfo_reqheight())
+    out.config(bg='#333333', fg='#249903')
+    out.pack(fill=X)
+
+    out.insert('1.0', '\n\n---- AntLR com Python: -----\nAlunos:\nIsaac Silva Santos Ramos\nPedro Gonçalves Neto')
+    bar = Menu(Menu(window), tearoff=0)
+    bar.add_command(label='Open', command=abrirAqruivo)
+    bar.add_command(label='Analyze', command=analise)
+    bar.add_command(label='Quit', command=exit)
+
+
+    sizegrip = ttk.Sizegrip(window)
+    sizegrip.pack(side="right", anchor=SE)
+    #sizegrip.grid(row=1, sticky=SE)
+
+    window.config(menu=bar)
+    window.mainloop()
+
 
