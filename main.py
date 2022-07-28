b5264e941ef03f75e774b3b2cfdf682948ede268
Isaac Ramos <46172558+IsaacRamos1@users.noreply.github.com>
2022-07-28 20:22:19 -0300

    comentarios

--- main.py
+++ main.py
@@ -8,27 +8,30 @@ def abrirAqruivo():
     path = askopenfilename(filetypes=[('Python Files', '*.py'), ('Text Files', '*.txt')])
     if path == '':
         return
-    with open(path, 'r') as file:
+    with open(path, 'r') as file:           # Open => abrir o diretório para abrir um código existente
         code = file.read()
         editorText.delete('1.0', END)
         editorText.insert('1.0', code)
 
 
 def analise():
+    out.delete('1.0', END)
     code = editorText.get('1.0', END)
-    data = InputStream(code)
+    data = InputStream(code)                # "enviar" a entrada pra arquivo .g4
     lexer = expr2022(data)
     tokens = CommonTokenStream(lexer)
-    list_tokens = lexer.getAllTokens()
+    list_tokens = lexer.getAllTokens()     # vamos receber a lista de tokens
 
     file = open('lexica.txt', 'w+')
-    file.write("============================\n")
+    file.write("============================\n")            # abrir ou criar um arquivo txt
     file.write("{:<15} {:<8}\n".format('Token', 'Type'))
     file.write("============================\n")
 
     for token in list_tokens:
-        nomeEtoken = expr2022.symbolicNames[token.type]
-        file.write("\n{:15} {:<8}".format(token.text, nomeEtoken))
+        nomeEtoken = expr2022.symbolicNames[token.type]             # get nome e token
+        file.write("\n{:15} {:<8}".format(token.text, nomeEtoken))  # escrever no .txt
+
+    out.insert('1.0', '\n\n---- AntLR com Python: -----\nAlunos:\nIsaac Silva Santos Ramos\nPedro Gonçalves Neto')
 
     file.seek(0)
     out.insert('1.0', 'Process finished with exit code 0\n' + file.read())
@@ -37,26 +40,23 @@ def analise():
 
 if __name__ == '__main__':
     window = Tk()
-    window.title('Lexer Analyzer')
-    window.geometry('500x500')
+    window.iconbitmap('pusheen.ico')
+    window.title('Lexer Analyzer')          # objeto Interface
+    window.geometry('700x700')
     window.resizable(True, True)
 
-    #print(window.winfo_reqheight())
-    #print(window.winfo_reqheight(), window.winfo_reqwidth())
-    editorText = Text(height=10, font=("Consolas", 18))
-    editorText.config(bg='#333333', fg='#b08102', insertbackground='white')
+    editorText = Text(height=20, font=("Consolas", 18))
+    editorText.config(bg='#333333', fg='#b08102', insertbackground='white')  # área de código/entrada
     editorText.pack(fill=X)
 
     out = Text(height=window.winfo_reqheight())
-    out.config(bg='#333333', fg='#249903')
+    out.config(bg='#333333', fg='#249903')          # área de saída
     out.pack(fill=X)
 
-    out.insert('1.0', '\n\n---- AntLR com Python: -----\nAlunos:\nIsaac Silva Santos Ramos\nPedro Gonçalves Neto')
     bar = Menu(Menu(window), tearoff=0)
-    bar.add_command(label='Open', command=abrirAqruivo)
+    bar.add_command(label='Open', command=abrirAqruivo)     # Open Analyzer Quit
     bar.add_command(label='Analyze', command=analise)
     bar.add_command(label='Quit', command=exit)
-
 
     sizegrip = ttk.Sizegrip(window)
     sizegrip.pack(side="right", anchor=SE)
